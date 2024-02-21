3153a9143101db86b2fef8ca00b8a7ea375614f9
Nihita Lele <124602601+NihitaLele@users.noreply.github.com>
2024-02-21 12:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
-
+    let inps = document.querySelectorAll("input")
+	let first_name = inps[0].value
+	let last_name = inps[1].value
+     let full_name = first_name + " " + last_name
+	alert(full_name)
 }
